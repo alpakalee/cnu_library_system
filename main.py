a34d8e3a36cbbc7324fcd9abf903eb9f3732e373
tjwns1400 <tjwns1400@gmail.com>
2022-05-31 10:04:30 +0900

    회원목록(기능구현)

--- main.py
+++ main.py
@@ -12,7 +12,7 @@
 
 import sys
 import service.BookService as bookService
-
+import service.MemberService as memberService
 # BookService (실제) -> bookService (사용)
 
 while True:
@@ -49,7 +49,7 @@ while True:
     elif select_num == 4: # 도서 반납
         pass
     elif select_num == 5: # 회원 목록
-        pass
+        memberService.get_members()
     elif select_num == 6: # 회원 검색
         pass
     elif select_num == 7: # 프로그램 종료
